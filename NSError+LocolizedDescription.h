35a73b6bd25d8f4d820f5eef01e6af7b3ef64cb4
bandianhong <scottban@126.com>
2019-05-26 11:08:21 +0800

    Release 0.0.2

--- NSError+LocolizedDescription.h
+++ NSError+LocolizedDescription.h
@@ -1,7 +1,7 @@
 /**
  MIT License
  
- Copyright (c) 2018 Scott Ban (https://github.com/reference/IOSOpenApi)
+ Copyright (c) 2018 Scott Ban (https://github.com/reference/NSError-LocolizedDescription)
  
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
